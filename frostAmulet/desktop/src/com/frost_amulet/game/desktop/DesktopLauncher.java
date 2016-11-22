9eff5f4d45bd56383d1e67c55367c863fbfd4a27
whenyathima <100595878@student.swin.edu.au>
2016-11-22 21:17:28 +1100

    Add files via upload
    uploaded the libGTX files

---
+++ frostAmulet/desktop/src/com/frost_amulet/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.frost_amulet.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.frost_amulet.game.FrostAmulet;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FrostAmulet(), config);
+	}
+}
