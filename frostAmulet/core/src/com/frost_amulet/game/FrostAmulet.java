93c179f2b986a73c76baf9a6f9820e93172ef8d9
Sam Fitchett <samfitchett@Sams-MacBook-Air.local>
2016-12-04 21:33:02 +1100

    meh

--- frostAmulet/core/src/com/frost_amulet/game/FrostAmulet.java
+++ frostAmulet/core/src/com/frost_amulet/game/FrostAmulet.java
@@ -13,7 +13,7 @@ public class FrostAmulet extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("core/assets/badlogic.jpg");
 	}
 
 	@Override
